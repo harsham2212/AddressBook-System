6d2b2971f92a4be29df166b0d03c22c503c97631
Harsh Singh <kmrharsh50@gmail.com>
2021-11-16 17:12:07 +0530

    [Harsh]Added:Adding details about contact person Address-Book System Features Implemented

---
+++ AddressBook System/Program.cs
@@ -0,0 +1,39 @@
+﻿using Address_Book;
+using System;
+
+namespace AddressBook_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("Welcome to AddressBook Program");
+                    Console.WriteLine("Enter options : \n 1. Create Contacts \n 2. Exit");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email");
+                            ContactManager contact = new ContactManager()
+                            {
+                                FirstName = Console.ReadLine(),
+                                LastName = Console.ReadLine(),
+                                Address = Console.ReadLine(),
+                                City = Console.ReadLine(),
+                                State = Console.ReadLine(),
+                                Zip = Console.ReadLine(),
+                                PhoneNumber = Console.ReadLine(),
+                                Email = Console.ReadLine(),
+                            };
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+                    }
+                }
+            }
+        }
+    }
