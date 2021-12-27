e1fec5b62856faabe119b0006957e10e867141e7
Harsh Singh <kmrharsh50@gmail.com>
2021-12-27 12:39:52 +0530

    [Harsh]Added:Sort Address book by City,State or Zip AddressBookSysytem feature implemented

--- AddressBook System/NewContact.cs
+++ AddressBook System/NewContact.cs
@@ -171,6 +171,32 @@ namespace AddressBook_System
                 Console.WriteLine("The {0} Addressbook does not exist. Please Enter a Valid Addressbook Name. ", addressBookName);
             }
         }
-    }
- }
 
+        public void Sorting()
+         
+        {
+          Console.WriteLine("Enter the Address Book name that you want to sort : ");
+          string addressBookName = Console.ReadLine();
+          Console.WriteLine("How do you want the Sort the Addressbook : \n 1. Sort based on City \n 2. Sort based on State \n 3. Sort based on Zip");
+          int option = Convert.ToInt32(Console.ReadLine());
+           switch (option)
+              {
+                  case 1:
+                         dict[addressBookName].Sort((x, y) => x.City.CompareTo(y.City));
+                          Console.WriteLine("After Sorting alphabetically, The Address Book is arranged as : ");
+                          Display();
+                          break;
+                  case 2:
+                          dict[addressBookName].Sort((x, y) => x.State.CompareTo(y.State));
+                          Console.WriteLine("After Sorting alphabetically, The Address Book is arranged as : ");
+                          Display();
+                          break;
+                  case 3:
+                          dict[addressBookName].Sort((x, y) => x.Zip.CompareTo(y.Zip));
+                          Console.WriteLine("After Sorting alphabetically, The Address Book is arranged as : ");
+                          Display();
+                          break;
+               }
+          }
+     }
+}
